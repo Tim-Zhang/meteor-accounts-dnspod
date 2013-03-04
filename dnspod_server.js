6211eac73928585c979cdd18f81f121e0cbbfc7e
TimZhang <timzhang@dnspod.com>
2013-03-04 23:33:34 +0800

    finish 1

--- dnspod_server.js
+++ dnspod_server.js
@@ -1,7 +1,7 @@
 (function () {
   Accounts.oauth.registerService('dnspod', 2, function (query) {
     var config = Accounts.loginServiceConfiguration.findOne({
-      service: 'qq'
+      service: 'dnspod'
     });
     if (!config) {
       throw new Accounts.ConfigError("DNSPod AuthService not configured");
@@ -10,27 +10,24 @@
     var accessToken = getAccessToken(config, query);
     var identity = getIdentity(config, accessToken.accessToken);
     
-    return {
+    var returnVal = {
       serviceData: {
         id: identity.id,
-        accessToken: accessToken.accessToken
+        accessToken: accessToken.accessToken,
       },
       options: {
         profile: {
-          openId : identity.id,
-          name: identity.name,
-          figureUrl: identity.figureUrl,
-          figureUrlAt50 : identity.figureUrlAt50,
-          figureUrlAt100 : identity.figureUrlAt100,
-          level: identity.level
+          id : identity.id
         }
       }
     };
+    console.log(returnVal);
+    return returnVal;
     
   });
 
   var getAccessToken = function (config, query) {
-    var result = Meteor.http.get("https://www.dnspod.cn/OAuth/Access.Token", {
+    var result = Meteor.http.post("https://www.dnspod.cn/OAuth/Access.Token", {
       params: {
         code: query.code,
         client_id: config.clientId,
@@ -45,58 +42,39 @@
       throw result.error;
     }
 
-    var dnspodAccessToken;
-    _.each(result.content.split('&'), function (kvString) {
-      var kvArray = kvString.split('=');
-      if (kvArray[0] === 'access_token')
-        dnspodAccessToken = kvArray[1];
-    });
+    var content = JSON.parse(result.content);
+    var dnspodAccessToken = content.access_token;
+
     return {
       accessToken: dnspodAccessToken
     };
   };
 
   var getIdentity = function (config, accessToken) {
-    var meResult = Meteor.http.get("https://www.dnspod.cn/User.Detail", {
+
+    var userInfoResult = Meteor.http.post("https://dnsapi.cn/User.Detail", {
       params: {
+        format: 'json',
         access_token: accessToken
       }
     });
-
-    // The response content in /me requires trickly JSONP callback to parse
-    var meContent = {};
-    var callbackExp = /^\s*callback\s*\((.+)\)\s*;\s*$/;
-    var matched = meResult.content.match(callbackExp);
-    if (matched && matched.length === 2) {
-      meContent = JSON.parse(matched[1]);
-      if (meContent.error) {
-        console.log("Error in getting account's open id, details: " + meContent.error);
-        throw new Error(meContent.error);
-      }
-    } else {
-      throw new Error("Error in getting account's open id");
-    }
-
-    var userInfoResult = Meteor.http.get("https://www.dnspod.cn/User.Detail", {
-      params: {
-        access_token: accessToken,
-        oauth_consumer_key: config.clientId,
-        openid: meContent.openid
-      }
-    });
     var userInfoContent = JSON.parse(userInfoResult.content);
+    var user
+    if (userInfoContent && userInfoContent.status && userInfoContent.status.code === "1") {
+       user = userInfoContent.info.user;
+    } else {
+      console.log("Error in getting account's user information, details: " + userInfoContent.msg);
+      throw new Error(userInfoContent.msg);
+    }
     if (userInfoContent.ret) {// 'ret' > 0
       console.log("Error in getting account's user information, details: " + userInfoContent.msg);
       throw new Error(userInfoContent.msg);
     }
 
     return {
-      id: meContent.openid,
-      name: userInfoContent.nickname,
-      figureUrl: userInfoContent.figureurl,
-      figureUrlAt50: userInfoContent.figureurl_1,
-      figureUrlAt100: userInfoContent.figureurl_2,
-      level: userInfoContent.level
+      id: user.id,
+      name: user.nick,
+      real_name: user.real_name
     };
   };
 })();
--- package.js
+++ package.js
@@ -6,7 +6,11 @@ Package.on_use(function(api) {
   api.use('accounts-base', ['client', 'server']);
   api.use('accounts-oauth2-helper', ['client', 'server']);
   api.use('http', ['client', 'server']);
+  api.use('templating', 'client');
 
+  api.add_files(
+         ['dnspod_login_button.css', 'dnspod_configure.html', 'dnspod_configure.js'],
+             'client');
   api.add_files('dnspod_common.js', ['client', 'server']);
   api.add_files('dnspod_server.js', 'server');
   api.add_files('dnspod_client.js', 'client');
